6a3f84e48d8c95377aee0f83c1b156aa5bda2a68
Mh Mitas <mahfuzulmitas@gmail.com>
2024-06-05 15:51:44 +0600

    commit-takend data from update forme

--- src/components/dashboard/modals/UpdateStudySessionModal.jsx
+++ src/components/dashboard/modals/UpdateStudySessionModal.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import useAuth from '../../../hooks/useAuth';
 import { useForm } from 'react-hook-form';
 import Heading from '../../common/Heading';
 import { format } from "date-fns";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import uploadImage from '../../../utils/uploadImage';
 
 // this is for admin
 const UpdateStudySessionModal = ({ session, setShowModal }) => {
@@ -13,7 +16,7 @@ const UpdateStudySessionModal = ({ session, setShowModal }) => {
     const { user } = useAuth()
     const { register, handleSubmit, reset } = useForm()
     // destructuring from loaded detail data
-    const { session_title, thumbnail_image, tutor_email, tutor_name, registrationDuration, registration_fee, classDuration, session_description } = session;
+    const { session_title, thumbnail_image: old_image, tutor_email, tutor_name, registrationDuration, registration_fee, classDuration, session_description } = session;
 
     const handleOverlayClick = (e) => {
         if (e.target === e.currentTarget) {
@@ -21,14 +24,40 @@ const UpdateStudySessionModal = ({ session, setShowModal }) => {
         }
     };
 
-    console.log(format(registrationDuration?.regStart, 'yyyy-dd-mm'));
+    async function onSubmit(data, event) {
+        const form = event.target;
+        const regStart = form.regStart.value;
+        const regEnd = form.regEnd.value;
+        const startDate = form.startDate.value;
+        const endDate = form.endDate.value;
+        const registrationDuration = { regStart, regEnd }
+        const classDuration = { startDate, endDate }
+
+        let thumbnail_image = old_image
+        try {
+            // upload img in img-bb
+            if (data.thumbnail_image[0]) {
+                const imgBbRes = await uploadImage(data.thumbnail_image[0])
+                thumbnail_image = imgBbRes
+                console.log(thumbnail_image);
+            }
+            const sessionInfo = { ...data, classDuration, registrationDuration, thumbnail_image }
+            // post on database
+            console.table(sessionInfo);
+
+        } catch (err) {
+            console.error(err);
+        }
+
+        // console.table({ ...data, classDuration, registrationDuration, status: 'pending', thumbnail_image });
+    }
 
     return (
         <div onClick={handleOverlayClick} className='fixed inset-0 flex justify-center items-center bg-black bg-opacity-40 z-50'>
             <section className='shadow-xl w-[90vw] max-w-screen-lg rounded-md mx-auto bg-base-100'>
                 <div className='max-w-screen-lg mx-auto rounded-t-md overflow-x-auto max-h-[90vh] overflow-y-auto bg-base-100 p-4'><Heading heading="Update Study Session" /></div>
 
-                <form className='grid grid-cols-1 sm:grid-cols-2 gap-6 bg-base-100 p-8 max-w-screen-lg mx-auto rounded-md overflow-x-auto max-h-[90vh] overflow-y-auto z-50'>
+                <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-1 sm:grid-cols-2 gap-6 bg-base-100 p-8 max-w-screen-lg mx-auto rounded-md overflow-x-auto max-h-[90vh] overflow-y-auto z-50'>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Tutor Name</span>
@@ -60,33 +89,30 @@ const UpdateStudySessionModal = ({ session, setShowModal }) => {
                         <input defaultValue={registration_fee} readOnly type="text" className="input input-bordered" required />
                     </div>
                     {/* registration duration */}
-
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text ">Registration Start Date</span>
                         </label>
-                        <input defaultValue={format(registrationDuration?.regStart, 'yyyy-dd-mm')} name="regStart" type="date" className="input input-bordered" />
+                        <input defaultValue={getDefaultDate(registrationDuration?.regStart)} required name="regStart" type="date" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text ">Registration End Date</span>
                         </label>
-                        <input name="regEnd" type="date" className="input input-bordered" />
+                        <input defaultValue={getDefaultDate(registrationDuration?.regEnd)} required name="regEnd" type="date" className="input input-bordered" />
                     </div>
-                    {/* <div className="divider divider-horizontal hidden md:flex"></div> */}
-                    {/* class start to end date duration */}
-
+                    {/* class duration */}
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text ">Class Start Date</span>
                         </label>
-                        <input name="endDate" type="date" className="input input-bordered" />
+                        <input defaultValue={getDefaultDate(classDuration?.startDate)} required name="endDate" type="date" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text ">Class End Date</span>
                         </label>
-                        <input name="startDate" type="date" className="input input-bordered" />
+                        <input defaultValue={getDefaultDate(classDuration?.endDate)} required name="startDate" type="date" className="input input-bordered" />
                     </div>
                     <div className='text-center w-full'>
                         <label className="form-control w-full">
@@ -100,7 +126,7 @@ const UpdateStudySessionModal = ({ session, setShowModal }) => {
                         <label className="label">
                             <span className="label-text">Session Description</span>
                         </label>
-                        <textarea required {...register('session_description')} className="textarea textarea-bordered" placeholder="About session"></textarea>
+                        <textarea defaultValue={session_description} required {...register('session_description')} className="textarea textarea-bordered" placeholder="About session"></textarea>
                     </div>
 
                     <div className='text-center w-full sm:col-span-2 mt-8'>
@@ -113,3 +139,17 @@ const UpdateStudySessionModal = ({ session, setShowModal }) => {
 };
 
 export default UpdateStudySessionModal;
+
+// const [regStartDate, setRegStartDate] = useState(new Date(registrationDuration?.regStart));
+// const [regEndDate, setRegEndDate] = useState(new Date(registrationDuration?.regEndDate));
+// const [classStartDate, setClassStartDate] = useState(new Date(classDuration?.startDate));
+// const [classEndDate, setClassEndDate] = useState(new Date(classDuration?.endDate));
+
+function getDefaultDate(date) {
+    const today = new Date(date)
+    const year = today.getFullYear()
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const defaultValue = `${year}-${month}-${day}`;
+    return defaultValue
+}

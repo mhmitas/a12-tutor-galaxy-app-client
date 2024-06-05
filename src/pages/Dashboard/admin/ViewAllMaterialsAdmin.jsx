ee383ff6d3e493b260dcf5e8bd6f97bbb589a3e0
Mh Mitas <mahfuzulmitas@gmail.com>
2024-06-05 11:02:25 +0600

    commit-

--- src/pages/Dashboard/admin/ViewAllMaterialsAdmin.jsx
+++ src/pages/Dashboard/admin/ViewAllMaterialsAdmin.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Heading from '../../../components/common/Heading';
+import useGetQuery from '../../../hooks/useGetQuery';
 
 const ViewAllMaterialsAdmin = () => {
+    const [data, isLoading, refetch, error] = useGetQuery('all-study-materials-admin', '/all-materials')
+    console.log(data);
     return (
         <div>
             <Heading heading={'All study Materials'} />
